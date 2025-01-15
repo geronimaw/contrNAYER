3bd9e6f11d48166df48eb798eb18a7ca33d1133c
geronimaw <devcontainers@AleMSI>
2025-01-15 10:35:12 +0100

    Remove handling of hyperparamters as list

--- datafree_kd.py
+++ datafree_kd.py
@@ -135,7 +135,6 @@ parser.add_argument('--new_aug', default=False, type=bool)
 parser.add_argument('--contr_loss', default="KLD", type=str)
 parser.add_argument('--temperature', default=2, type=float)
 parser.add_argument('--contr_on_stud', default=False, type=bool)
-parser.add_argument('--hyperparams', default=None)
 
 best_acc1 = 0
 time_cost = 0
@@ -201,15 +200,6 @@ def main_worker(gpu, ngpus_per_node, args):
         args.autocast = autocast
     else:
         args.autocast = datafree.utils.dummy_ctx
-
-    ############################################
-    # HYPERPARAMETERS for consistency constraint
-    if args.hyperparams is not None:
-        hyperparams = args.hyperparams.split(",")
-        args.contr_loss = float(hyperparams[0])
-        args.temperature = float(hyperparams[1])
-        args.new_aug = bool(hyperparams[2])
-    ############################################
 
     ############################################
     # Logger
